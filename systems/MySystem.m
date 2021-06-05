d38e99fc20b3a14c2439fa144df3335e4d26399f
unknown <snuvistasy@gmail.com>
2021-06-05 14:22:47 +0900

    Add a plot method for the example system (#8)

--- systems/MySystem.m
+++ systems/MySystem.m
@@ -15,9 +15,32 @@ classdef MySystem < SequentialSystem
             obj.attachDynSystems({obj.linearSystem});
         end
         
+        % implement
         function forward(obj)
             u_step = 1;
             obj.linearSystem.forward(u_step);
+        end
+        
+        function [fig, ax] = plot(obj, fig, ax)
+            if nargin < 2
+                fig = figure();
+            end
+            if nargin < 3
+                ax = axes('Parent', fig);
+            end
+            
+            history = obj.linearSystem.history;
+            [timeList, stateList] = history.get();
+            
+            figure(fig);
+            hold on
+            plot(ax, timeList, stateList(1, :), 'DisplayName', 'x1')
+            plot(ax, timeList, stateList(2, :), 'DisplayName', 'x2')
+            xlabel('Time')
+            ylabel('State')
+            grid on
+            box on
+            legend()
         end
     end
 end
