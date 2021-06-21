53796cff73c1f9b577d013afce5d823fe6cfcf9f
unknown <snuvistasy@gmail.com>
2021-06-21 23:30:42 +0900

    Remove step and propagate methods from MultipleSystem class and fix a bug (#24)

--- core/systems/MultipleSystem.m
+++ core/systems/MultipleSystem.m
@@ -48,65 +48,29 @@ classdef(Abstract) MultipleSystem < BaseSystem
         end
         
         % override
-        function out = stateDeriv(obj, stateFeed, timeFeed)
-            if isempty(obj.systemList)
-                error('Attach dynamic systems first')
-            end
+        function out = stateDeriv(obj, stateFeed, timeFeed, varargin)
+            assert(~isempty(obj.systemList), 'Attach dynamic systems first')
             if nargin < 3
                 stateFeed = [];
                 timeFeed = [];
             end
-            applyTime(obj, timeFeed);
-            out = stateDeriv@BaseSystem(obj, stateFeed, timeFeed);
+            if ~isempty(timeFeed)
+                applyTime(obj, timeFeed);
+            end
+            out = stateDeriv@BaseSystem(obj, stateFeed, timeFeed, varargin{:});
         end
         
         % override
         function applyTime(obj, timeFeed)
             obj.time = timeFeed;
             for k = 1:numel(obj.systemList)
-                obj.systemList{k}.time = timeFeed;
+                obj.systemList{k}.applyTime(timeFeed);
             end
         end
         
         function saveHistory(obj)
             for k = 1:numel(obj.systemList)
                 obj.systemList{k}.saveHistory();
-            end
-        end
-        
-        function step(obj, dt, saveHistory, varargin)
-            if nargin < 3 || isempty(saveHistory)
-                saveHistory = true;
-            end
-            obj.forward(varargin{:});
-            if saveHistory
-                obj.saveHistory();
-            end
-            
-            % remember initial state values
-            t0 = obj.time;
-            y0 = obj.state;
-            
-            k1 = obj.stateDeriv();
-            k2 = obj.stateDeriv(y0 + dt/2*k1, t0 + dt/2);
-            k3 = obj.stateDeriv(y0 + dt/2*k2, t0 + dt/2);
-            k4 = obj.stateDeriv(y0 + dt*k3, t0 + dt);
-            
-            % update time and states
-            t = t0 + dt;
-            y = y0 + dt*(k1 + 2*k2 + 2*k3 + k4)/6;
-            obj.applyTime(t);
-            obj.applyState(y);
-            obj.time = t;
-        end
-        
-        function propagate(obj, dt, time, saveHistory, varargin)
-            if nargin < 4 || isempty(saveHistory)
-                saveHistory = true;
-            end
-            iterNum = round(time/dt);
-            for i = 1:iterNum
-                step(obj, dt, saveHistory, varargin{:});
             end
         end
     end
