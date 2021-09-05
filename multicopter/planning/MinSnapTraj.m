bb786fae5d21b0c576a5b532c3a1b95294fe9ec6
SangminLee <snuvistasy@gmail.com>
2021-09-05 21:44:14 +0900

    Modify MinSnapTraj to offer time derivatives of the trajectory (#109)

--- multicopter/planning/MinSnapTraj.m
+++ multicopter/planning/MinSnapTraj.m
@@ -8,10 +8,10 @@ classdef MinSnapTraj < BaseFunction
         sigma_offset
         sigma_scale
         
-        c_x
-        c_y
-        c_z
-        c_psi
+        c_sigma_0 % coefficients for the 0-th derivative of sigma
+        c_sigma_1 % coefficients for the 1-th derivative of sigma
+        c_sigma_2 % coefficients for the 2-nd derivative of sigma
+        c_sigma_3 % coefficients for the 3-rd derivative of sigma
     end
     methods
         function obj = MinSnapTraj(t_key, sigma_key, c_sigma)
@@ -33,31 +33,76 @@ classdef MinSnapTraj < BaseFunction
             obj.sigma_scale = ...
                 max(sigma_key, [], 2) - min(sigma_key, [], 2) + eps(1);
             
-            [obj.c_x, obj.c_y, obj.c_z, obj.c_psi] = c_sigma{:};
+            obj.c_sigma_0 = c_sigma;
+            obj.c_sigma_1 = cell(size(c_sigma));
+            obj.c_sigma_2 = cell(size(c_sigma));
+            obj.c_sigma_3 = cell(size(c_sigma));
+            for j = 1:4
+                c_j_0 = c_sigma{j};
+                c_j_1 = Polynomial.polyder(c_j_0);
+                c_j_2 = Polynomial.polyder(c_j_1);
+                c_j_3 = Polynomial.polyder(c_j_2);
+                
+                obj.c_sigma_1{j} = c_j_1;
+                obj.c_sigma_2{j} = c_j_2;
+                obj.c_sigma_3{j} = c_j_3;
+            end
         end
         
-        function sigma = forward(obj, time)
+        function out = forward(obj, time)
             m = numel(obj.t_key) - 1;
             t_0 = obj.t_key(1);
             t_m = obj.t_key(end);
             
-            time(time < t_0) = t_0;
-            time(time > t_m) = t_m;
-            
-            t_tilde = (time - obj.t_offset)/obj.t_scale;
-            sigma_tilde = zeros(4, numel(t_tilde));
-            for j = 1:numel(t_tilde)
-                ind = find(obj.t_key > time(j), 1, 'first') - 1;
-                if isempty(ind) || (ind > m)
-                    ind = m;
-                end
-                x_tilde = polyval(obj.c_x(ind, :), t_tilde(j));
-                y_tilde = polyval(obj.c_y(ind, :), t_tilde(j));
-                z_tilde = polyval(obj.c_z(ind, :), t_tilde(j));
-                psi_tilde = polyval(obj.c_psi(ind, :), t_tilde(j));
-                sigma_tilde(:, j) = [x_tilde; y_tilde; z_tilde; psi_tilde];
+            if time < t_0
+                time = t_0;
             end
-            sigma = sigma_tilde.*obj.sigma_scale + obj.sigma_offset;
+            if time > t_m
+                time = t_m;
+            end
+            
+            ind = find(obj.t_key > time, 1, 'first') - 1;
+            if isempty(ind) || (ind > m)
+                ind = m;
+            end
+            tNorm = (time - obj.t_offset)/obj.t_scale;
+            sigmaNorm_0 = zeros(4, 1);
+            sigmaNorm_1 = zeros(4, 1);
+            sigmaNorm_2 = zeros(4, 1);
+            sigmaNorm_3 = zeros(4, 1);
+            for j = 1:4
+                c_j_0 = obj.c_sigma_0{j};
+                c_j_1 = obj.c_sigma_1{j};
+                c_j_2 = obj.c_sigma_2{j};
+                c_j_3 = obj.c_sigma_3{j};
+                
+                c_ij_0 = c_j_0(ind, :);
+                c_ij_1 = c_j_1(ind, :);
+                c_ij_2 = c_j_2(ind, :);
+                c_ij_3 = c_j_3(ind, :);
+                
+                sigmaNorm_0(j) = polyval(c_ij_0, tNorm);
+                sigmaNorm_1(j) = polyval(c_ij_1, tNorm);
+                sigmaNorm_2(j) = polyval(c_ij_2, tNorm);
+                sigmaNorm_3(j) = polyval(c_ij_3, tNorm);
+            end
+            sigma = sigmaNorm_0.*obj.sigma_scale + obj.sigma_offset;
+            sigma_dot = sigmaNorm_1.*obj.sigma_scale*(1/obj.t_scale);
+            sigma_2dot = sigmaNorm_2.*obj.sigma_scale*(1/obj.t_scale)^2;
+            sigma_3dot = sigmaNorm_3.*obj.sigma_scale*(1/obj.t_scale)^3;
+            
+            out = DerivVariable(sigma, sigma_dot, sigma_2dot, sigma_3dot);
+        end
+        
+        function var_p = posTrajectory(obj, time)
+            var_sigma = obj.forward(time);
+            var_p = var_sigma.get(1:3);
+        end
+        
+        function var_psi = headTrajectory(obj, time)
+            var_sigma = obj.forward(time);
+            temp = var_sigma.get(4);
+            var_psi = DerivVariable(temp.deriv(0), temp.deriv(1));
         end
         
         function figs = plot(obj, figs)
@@ -95,8 +140,12 @@ classdef MinSnapTraj < BaseFunction
                 t_high = obj.t_key(i + 1);
                 t_low = obj.t_key(i);
                 
-                timeList = linspace(t_low, t_high);
-                sigmaList = obj.forward(timeList);
+                timeList = linspace(t_low, t_high, 20);
+                sigmaList = zeros(4, numel(timeList));
+                for j = 1:numel(timeList)
+                    var_sigma = obj.forward(timeList(j));
+                    sigmaList(:, j) = var_sigma.deriv(0);
+                end
                 
                 figure(figs{1})
                 hold on
