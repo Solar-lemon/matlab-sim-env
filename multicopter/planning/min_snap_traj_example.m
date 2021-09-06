c803b411789099a54003535e38dad01235936123
SangminLee <snuvistasy@gmail.com>
2021-09-06 15:05:04 +0900

    Modify plot methods (#109)

--- multicopter/planning/min_snap_traj_example.m
+++ multicopter/planning/min_snap_traj_example.m
@@ -6,11 +6,11 @@ addpath(genpath('../../common'))
 
 n = [7, 7, 7, 4]; % n_pos = 7, n_psi = 4
 k = [4, 4, 4, 2]; % k_pos = 4, k_psi = 2;
-t_key = [0, 2.5, 5];
+t_key = [0, 10, 20];
 sigma_key = [...
     [0; 0; 0; 0],...
-    [0.3; 0.5; 0.2; deg2rad(5)],...
-    [1; 1; 0.5; deg2rad(10)]];
+    [3; 5; 2; deg2rad(5)],...
+    [10; 10; 5; deg2rad(10)]];
 
 trajGeneration = MinSnapTrajGeneration(n, k, t_key, sigma_key);
 traj = trajGeneration.generateTraj();
@@ -24,7 +24,7 @@ posTrajFun = DiscreteFunction(@(t) optTimeTraj.posTrajectory(t), 1/100); % 100 [
 headTrajFun = DiscreteFunction(@(t) optTimeTraj.headTrajectory(t), 1/100); % 100 [Hz]
 
 model = GeoPosTracking();
-Simulator(model).propagate(0.01, 5, true, posTrajFun, headTrajFun);
-model.quadrotor.plot();
+Simulator(model).propagate(0.001, 20, true, posTrajFun, headTrajFun);
+model.plot();
 
 rmpath(genpath('../../common'))
