c2cef7ff91d2ac7a59c96ef46ff704998c4758bc
SangminLee <snuvistasy@gmail.com>
2021-11-06 20:41:40 +0900

    Modify TimeVaryingDynSystem and DynSystem (#136)

--- core/systems/TimeVaryingDynSystem.m
+++ core/systems/TimeVaryingDynSystem.m
@@ -16,151 +16,121 @@ classdef TimeVaryingDynSystem < BaseSystem
             if nargin < 2
                 derivFun = [];
             end
+            
             obj = obj@BaseSystem(initialState);
             obj.name = 'timeVaryingDynSystem';
             obj.initialState = initialState;
             
-            if isempty(derivFun)
-                derivFun = @obj.derivative;
+            if isa(derivFun, 'SimObject')
+                obj.derivFun = @derivFun.forward;
+            else
+                obj.derivFun = derivFun;
             end
-            obj.derivFun = derivFun;
+            
             obj.outputFun = outputFun;
         end
         
         % override
-        function reset(obj, initialState)
-            if nargin < 2
-                initialState = obj.initialState;
-            end
+        function reset(obj)
             reset@BaseSystem(obj);
-            obj.stateVarList{1}.state = initialState;
+            obj.stateVarList.get(1).applyState(obj.initialState);
         end
         
-        function attachDerivFun(obj, derivFun)
-            % derivFun: function_handle or BaseFunction
-            % derivFun(state, time, input1, ..., inputM)
-            obj.derivFun = derivFun;
-        end
-        
-        function attachOutputFun(obj, outputFun)
-            % outputFun: function_handle or BaseFunction
-            % outputFun(state, time)
-            obj.outputFun = outputFun;
+        % override
+        function setState(obj, state)
+            obj.stateVarList.get(1).applyState(state);
         end
         
         % to be implemented
-        function out = derivative(obj, varargin)
-            % implement this method if needed
-            % varargin: {state, time, input1, ..., inputM}
-            % out: derivState
-            fprintf("Attach a derivFun or implement the derivative method! \n")
-            out = zeros(size(obj.initialState));
+        function out = derivative(obj, time, state, varargin)
+            assert(~isempty(obj.derivFun),...
+                "Attach a derivFun or implement the derivative method!")
+            out = obj.derivFun(time, state, varargin{:});
         end
         
         % implement
         function out = forward(obj, varargin)
             % varargin: {input1, ..., inputM}
-            if isa(obj.derivFun, 'BaseFunction')
-                deriv = obj.derivFun.forward(...
-                    obj.stateVarList{1}.state, obj.time, varargin{:});
-                obj.stateVarList{1}.forward(deriv);
-            else
-                deriv = obj.derivFun(...
-                    obj.stateVarList{1}.state, obj.time, varargin{:});
-                obj.stateVarList{1}.forward(deriv);
+            deriv = obj.derivative(...
+                obj.simClock.time, obj.stateVarList.get(1).state, varargin{:});
+            obj.stateVarList.get(1).forward(deriv);
+            
+            if obj.logTimer.isEvent
+                obj.logger.append(...
+                    {'time', 'state'},...
+                    {obj.simClock.time, obj.stateVarList.get(1).state});
+                inputKeySet = cell(size(varargin));
+                for i = 1:numel(varargin)
+                    inputKeySet{i} = ['u_', num2str(i)];
+                end
+                obj.logger.append(inputKeySet, varargin);
             end
             
-            keySet = {'time', 'state'};
-            valueSet = {obj.simClock.time, obj.stateVarList{1}.state};
-            
-            inputKeySet = cell(size(varargin));
-            for i = 1:numel(varargin)
-                inputKeySet{i} = ['input', num2str(i)];
-            end
-            keySet = [keySet, inputKeySet];
-            valueSet = [valueSet, varargin];
-            
-            obj.logger.forward(keySet, valueSet);
-            
-            if nargout > 0
-                out = obj.output;
-            end
+            out = obj.output;
         end
         
         % implement
         function out = output(obj)
             % outputFun: function_handle or BaseFunction
             % outputFun(state)
-            if isa(obj.outputFun, 'BaseFunction')
-                out = obj.outputFun.forward(obj.stateVarList{1}.state, obj.time);
-            else
-                out = obj.outputFun(obj.stateVarList{1}.state, obj.time);
-            end
+            out = obj.outputFun(obj.simClock.time, obj.stateVarList.get(1).state);
         end
     end
     
     % set and get methods
     methods
         function out = get.stateVar(obj)
-            out = obj.stateVarList{1};
+            out = obj.stateVarList.get(1);
         end
         
         % override
         function out = stateFlatValue(obj)
-            out = reshape(obj.stateVarList{1}.state, [], 1);
+            out = reshape(obj.stateVarList.get(1).state, [], 1);
         end
     end
     
     methods(Access=protected)
         % override
         function out = getState(obj)
-            out = obj.stateVarList{1}.state;
+            out = obj.stateVarList.get(1).state;
         end
         
         % override
         function out = getDeriv(obj)
-            out = obj.stateVarList{1}.deriv;
+            out = obj.stateVarList.get(1).deriv;
         end
     end
     
     methods
-        function fig = plot(obj, fig)
-            if obj.stateNum > 4
-                return
-            end
+        function figs = plot(obj, varKeys)
             if nargin < 2
+                varKeys = List(obj.logger.keys());
+                varKeys.remove('time');
+            end
+            
+            figs = List();
+            timeList = obj.history('time');
+            
+            for i = 1:numel(varKeys)
+                varKey = varKeys.get(i);
                 fig = figure();
+                figs.append(fig);
+                
+                varList = obj.history(varKey);
+                ind = 1:size(varList, 1);
+                subplotNum = numel(ind);
+                for k = 1:subplotNum
+                    subplot(subplotNum, 1, k)
+                    hold on
+                    plot(timeList, varList(ind(k), :), 'DisplayName', "Actual")
+                    xlabel("Time [s]")
+                    ylabel(sprintf("%s(%d)", varKey, k))
+                    grid on
+                    box on
+                    legend()
+                end
+                sgtitle(varKey)
             end
-            
-            loggedData = obj.history('time', 'state', 'input1');
-            [timeList, stateList, controlList] = loggedData{:};
-            stateNum = obj.stateNum;
-            inputNum = size(controlList, 1);
-            
-            figure(fig);
-            subplot(2, 1, 1)
-            hold on
-            for k = 1:stateNum
-                plot(timeList, stateList(k, :), 'DisplayName', sprintf('x%d', k))
-            end
-            title('State')
-            xlabel('Time')
-            ylabel('State')
-            grid on
-            box on
-            legend()
-            
-            subplot(2, 1, 2)
-            hold on
-            for k = 1:inputNum
-                plot(timeList, controlList(k, :), 'DisplayName', sprintf('u%d', k))
-            end
-            title('Control input')
-            xlabel('Time')
-            ylabel('Control input')
-            grid on
-            box on
-            legend()
         end
     end
     
@@ -170,16 +140,24 @@ classdef TimeVaryingDynSystem < BaseSystem
             close all
             
             fprintf('== Test for TimeVaryingDynSystem == \n')
-            derivFun = @(x, t, u) -1/(1 + t)^2*x + u;
-            system = TimeVaryingDynSystem(1, derivFun);
+            dt = 0.01;
+            simClock = SimClock();
+            logTimer = Timer(dt);
+            logTimer.attachSimClock(simClock);
+            logTimer.turnOn();
+            
+            derivFun = @(t, x, u) -1/(1 + t)^2*x + u;
+            model = TimeVaryingDynSystem(1, derivFun);
+            model.attachSimClock(simClock);
+            model.attachLogTimer(logTimer);
             
             tic
             u_zero = 0;
-            Simulator(system).propagate(0.01, 10, true, u_zero);
+            model.propagate(0.01, 10, u_zero);
             elapsedTime = toc;
             
             fprintf('ElapsedTime: %.2f [s] \n', elapsedTime)
-            system.plot();
+            model.plot();
         end
     end
 end
